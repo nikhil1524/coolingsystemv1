c845074319d711a9c3a47b367b2ec6148219dad5
Nikhil <nikhil_n@live.com>
2013-06-30 21:55:16 +0530

    ok

--- CollingSystem/src/com/yeshtech/coolingsystem/layout/window/TimeCurveSpecification.java
+++ CollingSystem/src/com/yeshtech/coolingsystem/layout/window/TimeCurveSpecification.java
@@ -1,7 +1,11 @@
 package com.yeshtech.coolingsystem.layout.window;
 
+import java.awt.FlowLayout;
+
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Label;
@@ -19,16 +23,7 @@ public class TimeCurveSpecification {
 	protected Shell shell;
 	private Text txtCurveName;
 	private Text txtNumberOfRows;
-	private Text text_2;
-	private Text text_3;
-	private Text text_4;
-	private Text text_5;
-	private Text text_6;
-	private Text text_7;
-	private Text text_8;
-	private Text text_9;
-	private Text text_10;
-	private Text text_11;
+	
 
 	/**
 	 * Launch the application.
@@ -68,7 +63,7 @@ public class TimeCurveSpecification {
 		shell = new Shell();
 		shell.setSize(422, 436);
 		shell.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW));
-		
+		//shell.setLayout(new GridLayout(1,true));
 		Label lblCurveName = new Label(shell, SWT.NONE);
 		lblCurveName.setBounds(22, 18, 83, 15);
 		lblCurveName.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_CURVE_NAME));
@@ -87,8 +82,10 @@ public class TimeCurveSpecification {
 		btnCreateTable.setBounds(297, 13, 99, 45);
 		btnCreateTable.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_CREATE_TABLE));
 		
-		/*Composite composite = new Composite(shell, SWT.NONE);
-		composite.setBounds(35, 82, 336, 253);*/
+		
+		//Time Curve table started 
+		
+		
 		
 		/*Group grpTimeCurveTable = new Group(composite, SWT.NONE);
 		grpTimeCurveTable.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_TIMECURVE_TABLE));
@@ -96,69 +93,89 @@ public class TimeCurveSpecification {
 		
 		Group grpTimeCurveTable = new Group(shell,  SWT.NONE);
 		grpTimeCurveTable.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_TIMECURVE_TABLE));
-		grpTimeCurveTable.setBounds(20, 75, 365, 253);
-		grpTimeCurveTable.setLayout(new FillLayout(SWT.HORIZONTAL));
+		grpTimeCurveTable.setBounds(20, 84, 376, 261);
+		grpTimeCurveTable.setLayout(new FillLayout(SWT.VERTICAL));
+		//grpTimeCurveTable.setLayoutData(new GridData(SWT.FILL,SWT.FILL,false,true));
 		
-		ScrolledComposite frontComp = new ScrolledComposite(grpTimeCurveTable, SWT.H_SCROLL
-		        | SWT.V_SCROLL);
+		Composite composite = new Composite(grpTimeCurveTable, SWT.NONE);
+		composite.setBounds(35, 91, 336, 244);
+		composite.setLayout(new FillLayout(SWT.VERTICAL));
+		composite.setLayoutData(new GridData(SWT.FILL,SWT.FILL,false,true));
 		
-	    Composite innerFrontComp = new Composite(frontComp, SWT.NONE);
-	    innerFrontComp.setBounds(0, 0, 365, 253);
-		//frontComp.setAlwaysShowScrollBars(true);
+		ScrolledComposite srlComTable = new ScrolledComposite(composite,SWT.BORDER| SWT.V_SCROLL | SWT.H_SCROLL);
+		srlComTable.setLayout(new GridLayout(2,true));
+		srlComTable.setBounds(35,91,300,200);
+		
+//	    Composite innerFrontComp = new Composite(srlComTable, SWT.NONE);
+//	    innerFrontComp.setBounds(0, 0, 365, 253);
+//	    innerFrontComp.setLayout(new GridLayout(1,true));
+//
+//		
+//	    srlComTable.setMinSize(innerFrontComp.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+//	    srlComTable.setContent(innerFrontComp);
+//	    srlComTable.setExpandHorizontal(true);
+//	    srlComTable.setExpandVertical(true);
+//	    srlComTable.setAlwaysShowScrollBars(true);
+    	
+    	
+//		Label lblTimecurveTableXValues = new Label(innerFrontComp, SWT.NONE);
+//		lblTimecurveTableXValues.setBounds(67, 10, 55, 15);
+//		lblTimecurveTableXValues.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_X_VALUES));
+//		
+//		Label lblTimecurveTableYValues = new Label(innerFrontComp, SWT.NONE);
+//		lblTimecurveTableYValues.setBounds(212, 10, 55, 15);
+//		lblTimecurveTableYValues.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_Y_VALUES));
+//		
+//
+//    	for(int i = 0; i < 1000; i++){
+//    		new Button(innerFrontComp, SWT.PUSH).setText("Text");
+//    	}
+
 		
 		
 		
-		Label lblTimecurveTableXValues = new Label(innerFrontComp, SWT.NONE);
-		lblTimecurveTableXValues.setBounds(67, 10, 55, 15);
-		lblTimecurveTableXValues.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_X_VALUES));
+//		text_2 = new Text(innerFrontComp, SWT.BORDER);
+//		text_2.setBounds(32, 31, 125, 32);
+//		
+//		text_3 = new Text(innerFrontComp, SWT.BORDER);
+//		text_3.setBounds(182, 31, 125, 32);
+//		
+//		text_4 = new Text(innerFrontComp, SWT.BORDER);
+//		text_4.setBounds(32, 73, 125, 32);
+//		
+//		text_5 = new Text(innerFrontComp, SWT.BORDER);
+//		text_5.setBounds(182, 73, 125, 32);
 		
-		Label lblTimecurveTableYValues = new Label(innerFrontComp, SWT.NONE);
-		lblTimecurveTableYValues.setBounds(212, 10, 55, 15);
-		lblTimecurveTableYValues.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_Y_VALUES));
+//		text_6 = new Text(innerFrontComp, SWT.BORDER);
+//		text_6.setBounds(32, 118, 125, 32);
+//		
+//		text_7 = new Text(innerFrontComp, SWT.BORDER);
+//		text_7.setBounds(182, 118, 125, 32);
+//		
+//		text_8 = new Text(innerFrontComp, SWT.BORDER);
+//		text_8.setBounds(32, 166, 125, 32);
+//		
+//		text_9 = new Text(innerFrontComp, SWT.BORDER);
+//		text_9.setBounds(182, 166, 125, 32);
+//		
+//		text_10 = new Text(innerFrontComp, SWT.BORDER);
+//		text_10.setBounds(32, 208, 125, 32);
+//		
+//		text_11 = new Text(innerFrontComp, SWT.BORDER);
+//		text_11.setBounds(182, 208, 125, 32);
 		
-		text_2 = new Text(innerFrontComp, SWT.BORDER);
-		text_2.setBounds(32, 31, 125, 32);
-		
-		text_3 = new Text(innerFrontComp, SWT.BORDER);
-		text_3.setBounds(182, 31, 125, 32);
-		
-		text_4 = new Text(innerFrontComp, SWT.BORDER);
-		text_4.setBounds(32, 73, 125, 32);
-		
-		text_5 = new Text(innerFrontComp, SWT.BORDER);
-		text_5.setBounds(182, 73, 125, 32);
-		
-		text_6 = new Text(innerFrontComp, SWT.BORDER);
-		text_6.setBounds(32, 118, 125, 32);
-		
-		text_7 = new Text(innerFrontComp, SWT.BORDER);
-		text_7.setBounds(182, 118, 125, 32);
-		
-		text_8 = new Text(innerFrontComp, SWT.BORDER);
-		text_8.setBounds(32, 166, 125, 32);
-		
-		text_9 = new Text(innerFrontComp, SWT.BORDER);
-		text_9.setBounds(182, 166, 125, 32);
-		
-		text_10 = new Text(innerFrontComp, SWT.BORDER);
-		text_10.setBounds(32, 208, 125, 32);
-		
-		text_11 = new Text(innerFrontComp, SWT.BORDER);
-		text_11.setBounds(182, 208, 125, 32);
-		
+		//srlComTable.setContent(innerFrontComp);
 		Button btnCancel = new Button(shell, SWT.NONE);
 		btnCancel.setBounds(22, 354, 99, 34);
 		btnCancel.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_CANCEL));
 		
 		Button btnSave = new Button(shell, SWT.NONE);
-		btnSave.setBounds(274, 354, 99, 34);
+		btnSave.setBounds(297, 354, 99, 34);
 		btnSave.setText(loader.getValue(LabelConstants.LAYOUT_TIME_CURVE_SPECIFICATION_WINDOW_SAVE));
 
-		frontComp.setMinHeight(innerFrontComp.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
-	    frontComp.setMinWidth(innerFrontComp.computeSize(SWT.DEFAULT, SWT.DEFAULT).x);
-	    frontComp.setContent(innerFrontComp);
-	    frontComp.setExpandHorizontal(true);
-	    frontComp.setExpandVertical(true);
+		//frontComp.setMinHeight(innerFrontComp.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+	    //frontComp.setMinWidth(innerFrontComp.computeSize(SWT.DEFAULT, SWT.DEFAULT).x);
+	  
 	}
 
 }
